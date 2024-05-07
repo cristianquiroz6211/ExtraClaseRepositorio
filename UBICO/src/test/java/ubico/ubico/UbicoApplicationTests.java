47916219c43ef09ce07eedc15edeb10be554dde2
cristianquiroz6211 <Cristian.quiroz6211@uco.net.co>
2024-05-07 12:28:17 -0500

    Se crea repositorio Clase
    se crea el repo de clase junto al proyecto del sprongboot

---
+++ UBICO/src/test/java/ubico/ubico/UbicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ubico.ubico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
